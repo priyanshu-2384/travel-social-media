8484b1e2327d1890659662dc0f61d9f5e6bf62f2
Priyanshu Chaudhary <priyanshu2384chaudhary@gmail.com>
2024-06-15 13:06:28 +0530

    Fixed a database url bug

--- app.js
+++ app.js
@@ -32,8 +32,8 @@ app.use(express.urlencoded({ extended: true }));
 app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
-const dBUrl = MONGO_URL//process.env.ATLASDB_URL;
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";//
+const dBUrl = process.env.ATLASDB_URL;
 
 async function main() {
     await mongoose.connect(dBUrl);
